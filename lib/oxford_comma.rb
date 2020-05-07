e11e21c1f21e5c7f6730d62681376cac2edf3a54
Irene Scott <irenejinscott@gmail.com>
2020-05-07 14:36:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,3 +8,13 @@ if array > 3
 end
 
 
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
+end
