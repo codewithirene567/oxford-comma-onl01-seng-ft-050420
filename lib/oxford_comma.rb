f60be3f69fb1f2796d9a4fb335e7e3e6278e466e
Irene Scott <irenejinscott@gmail.com>
2020-05-07 14:54:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,12 +9,4 @@ def oxford_comma(array)
   end
 end
 
-def oxford_comma(array)
-[array].join
-[array].join(" and ")
-if array.length == 2
-        array.join(" and ")
-if array > 3
-  [array].join(", ")
-end
 
